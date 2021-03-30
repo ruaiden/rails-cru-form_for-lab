4658466e70ec08df3705d0771d65787f45f990b2
Aiden <aiden.marques94@gmail.com>
2021-03-29 22:12:46 -0400

    commit tree 04ae4a833fb9d3680bf894a8736df51feacc24a4

---
+++ db/migrate/001_create_artists.rb
@@ -0,0 +1,11 @@
+class CreateArtists < ActiveRecord::Migration
+
+    def change
+        create_table :artists do |t|
+            t.string :name
+            t.text :bio
+         
+        end
+    end 
+
+end 
